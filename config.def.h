bf3a0c28498bc89697ce70a7eb89124626c93887
Joe Smith <screwtape9@gmail.com>
2025-06-06 13:17:34 -0400

    Patched in dmenu-fuzzymatch-5.3.diff

--- config.def.h
+++ config.def.h
@@ -1,7 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 static int colorprompt = 1;                /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
@@ -9,15 +10,15 @@ static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the o
 static const unsigned int alpha = 200;     /* Amount of opacity. 255 (0xff) is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:style=Regular:size=12",
-	"JoyPixels:pixelsize=14:antialias=true:autohint=true"
+  "monospace:style=Regular:size=12",
+  "JoyPixels:pixelsize=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#96cdfb", "#000000" },
-	[SchemeSel] = { "#1e1d2d", "#96cdfb" },
-	[SchemeOut] = { "#000000", "#00ffff" }
+  [SchemeNorm] = { "#96cdfb", "#000000" },
+  [SchemeSel] = { "#1e1d2d", "#96cdfb" },
+  [SchemeOut] = { "#000000", "#00ffff" }
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
