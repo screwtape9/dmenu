a1efffec77f651003b8b7433f2447ca642c940a7
Joe Smith <screwtape9@gmail.com>
2025-06-06 13:14:00 -0400

    Ported in some phyOS color/opacity mods.

--- config.def.h
+++ config.def.h
@@ -6,23 +6,24 @@ static int colorprompt = 1;                /* -p  option; if 1, prompt uses Sche
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
-static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 200;     /* Amount of opacity. 255 (0xff) is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:style=Regular:size=12",
+	"JoyPixels:pixelsize=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#96cdfb", "#000000" },
+	[SchemeSel] = { "#1e1d2d", "#96cdfb" },
+	[SchemeOut] = { "#000000", "#00ffff" }
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
 	[SchemeSel] = { OPAQUE, alpha },
-	[SchemeOut] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha }
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
